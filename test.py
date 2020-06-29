3d0f257dd8308c41c48d5f26e068a5e8f4f051c8
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2020-06-29 15:38:21 +0800

    Added waiting interface.

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+bdo_personal_login = "https://online.bdo.com.ph/sso/login";
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+driver.get(bdo_personal_login)
+
+assert "Banco De Oro" in driver.title
+
+elem = driver.find_element_by_name("channelUserID")
 elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+elem.send_keys("darrensapalo")
+print("I've displayed your username, but you have to be the one to write the password.")
+try:
+    WebDriverWait(driver, 30).until(lambda x: 'Google' in driver.title or 'Facebook' in driver.title)
+
+    print("Oh wow, you're in google or facebook!")
+except TimeoutException as e:
+    print("You need to enter your password within 30 seconds.")
+    pass
+
+print("Redirecting you back to BDO.")
+driver.get(bdo_personal_login)
+
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
 # driver.close()
